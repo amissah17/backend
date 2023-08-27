1df9cf7e4bac269e694a1b1b1594ef8f033a3373
amissah17 <amissahfrank17@gmail.com>
2023-08-26 17:10:14 -0700

    production build

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 // const requestLogger = (request, response, next) => {
 //   console.log('Method:', request.method)
@@ -33,9 +34,9 @@ let notes = [
     }
   ]
 
-  // app.get('/', (request, response) => {
-  //   response.send('<h1>Hello World!</h1>')
-  // })
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
   
   app.get('/api/notes', (request, response) => {
     response.json(notes)
